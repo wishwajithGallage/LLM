53b25d0486c5d03e6af8360ff0071a3ff752871d
wishwajithGallage <112330401+wishwajithGallage@users.noreply.github.com>
2024-02-19 13:37:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if "chat_session" not in st.session_state:
 
 
 # Display the chatbot's title on the page
-st.title("🦾 WD Bing")
+st.title(🦾 "WD Bing" )
 st.title("This is WD Chatbot. It is based on Gemini API.Gemini is a powerful new family of large language models (LLMs) developed by Google DeepMind.")
 st.title("You can Click below text Field and you can Type what you want to know anything!")
 
