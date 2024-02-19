381bd9e57270ed34ce715b491c42ded8006d4bdc
wishwajithGallage <112330401+wishwajithGallage@users.noreply.github.com>
2024-02-19 13:07:58 +0530

    main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ if "chat_session" not in st.session_state:
 
 # Display the chatbot's title on the page
 st.title("🤖 WD Bing - ChatBot")
+st.title("🤖 WD Bing - ChatBot")
 
 # Display the chat history
 for message in st.session_state.chat_session.history:
